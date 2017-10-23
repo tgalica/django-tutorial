19c025074051d1717dd2c89d194b62b8900d3a2e
Tom <thomas.galica7@gmail.com>
2017-10-22 22:25:45 -0400

    before Unit Tests

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    #e.g. /polls/5/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    #e.g. /polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    #e.g. /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
